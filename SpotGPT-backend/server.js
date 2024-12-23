44e78047700a5ea287fd66d5cdf1d20d9963cf41
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-23 11:52:01 +0530

    mongo and cloudinary connected

--- SpotGPT-backend/server.js
+++ SpotGPT-backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+import connectCloudinary from './src/config/cloudinary.js';
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middleware
 
@@ -12,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 //initializing routes
+app.use("/api/song", songRouter);
+app.use("/api/list", songRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, This is the backend server');
