359e8cbf62a638591b87259c6511b09a14f7bca2
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-24 20:30:40 +0530

    list song completed

--- SpotGPT-Portal/src/pages/ListSong.jsx
+++ SpotGPT-Portal/src/pages/ListSong.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { url } from '../App';
+import { toast } from 'react-toastify';
 
 const ListSong = () => {
+
+  const [data, setData] = useState([]);
+  const fetchSongs = async() =>{
+    try{
+      const response = await axios.get(`${url}/api/song/list`);
+
+      if(response.data.success){
+        setData(response.data.songs);
+      }
+
+    } catch(error){
+      toast.error('Error Occured');
+    }
+  }
+
+  const removeSong = async(id) =>{
+    try{
+      const response = await axios.post(`${url}/api/song/remove`,{id});
+      
+      if(response.data.success){
+        toast.success('Song removed successfully');
+        fetchSongs();
+      }
+
+    } catch(error){
+      toast.error('Error Occured');
+    }
+  }
+
+  useEffect(()=>{
+    fetchSongs();
+  }, []);
+
   return (
-    <div>
-      List Song Page
+    <div className=''>
+      <p>All Songs List:</p>
+      <br />
+      <div>
+        <div className='sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Album</b>
+          <b>Duration</b>
+          <b>Action</b>
+        </div>
+
+        {data.map((item, index)=> {
+          return(
+            <div key={index} className='grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5'>
+              <img className='w-12' src={item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.album}</p>
+              <p>{item.duration}</p>
+              <p className='cursor-pointer' onClick={()=>removeSong(item._id)}> X </p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
