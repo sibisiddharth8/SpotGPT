6404aacc488f40791fc404c0ae9d520018489c8e
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-24 18:10:01 +0530

    sidebar and navbar added

--- SpotGPT-Portal/src/App.jsx
+++ SpotGPT-Portal/src/App.jsx
@@ -5,13 +5,17 @@ import AddSong from "./pages/AddSong";
 import AddAlbum from "./pages/addAlbum";
 import ListSong from "./pages/ListSong";
 import ListAlbum from "./pages/ListAlbum";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="flex items-start min-h-screen">
       <ToastContainer />
+      <Sidebar/>
 
       <div className="flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]">
+        <Navbar />
         <div className="pt-8 pl-5 sm:pt-12 sm:pl-12">
           <Routes>
             <Route path="/add-song" element={<AddSong />} />
