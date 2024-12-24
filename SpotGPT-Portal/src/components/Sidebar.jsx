e8b9b10b4bcef353b2742190d1afbc852a7d49da
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-24 21:27:04 +0530

    list album completed

--- SpotGPT-Portal/src/components/Sidebar.jsx
+++ SpotGPT-Portal/src/components/Sidebar.jsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='bg-[#003A10] min-h-screen pl-[4vw]'>
-        <img src={assets.logo} className='mt-5 w-[max(10vw, 100px)] hidden sm:block' alt="" />
-        <img src={assets.logo_small} className='mt-5 w-[max(5vw, 40px)] mr-5 sm:hidden block' alt="" />
+    <div className='bg-[#003A10] min-h-screen pl-[4vw] flex flex-col items-center'>
+        <img src={assets.logo_small} className='mt-5 w-[50px] hidden sm:block' alt="" />
+        <img src={assets.logo_small} className='mt-5 w-[max(5vw,40px)] mr-5 sm:hidden block' alt="" />
 
         <div className='flex flex-col gap-5 mt-10'>
             <NavLink to='/add-song' className='flex items-center gap-2.5 text-gray-800 bg-white border border-black p-3 drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
